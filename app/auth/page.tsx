320977bda0530c73835fe1e1ea8a509cd12c5cf2
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-11-06 22:39:27 -0300

    feat: add Github and Google login

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -10,6 +10,8 @@ import axios from "axios";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 export default function Auth() {
   const router = useRouter();
@@ -105,12 +107,47 @@ export default function Auth() {
             <button onClick={variant ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               {variant ? "Login" : "Sign up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn('google', { callbackUrl: '/' })}
+                className="
+                  w-10
+                  h-10
+                  bg-white
+                  rounded-full
+                  flex
+                  items-center
+                  justify-center
+                  cursor-pointer
+                  hover:opacity-80
+                  transition
+                "
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn('github', { callbackUrl: '/' })}
+                className="
+                  w-10
+                  h-10
+                  bg-white
+                  rounded-full
+                  flex
+                  items-center
+                  justify-center
+                  cursor-pointer
+                  hover:opacity-80
+                  transition
+                "
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant ? "First time using Netflix?" : "Already have an account?"}
               <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
                 {variant ? "Create an account" : "Login"}
               </span>
-
             </p>
           </div>
         </div>
